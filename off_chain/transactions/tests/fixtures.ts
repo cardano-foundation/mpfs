1e0c92e77691855c31f5969c56ce5f024f01c4d7
paolino <paolo.veronelli@gmail.com>
2025-06-02 13:32:06 +0100

    Update fixtures to run the indexer

--- off_chain/transactions/tests/fixtures.ts
+++ off_chain/transactions/tests/fixtures.ts
@@ -1,32 +1,58 @@
 import { generateMnemonic, MeshWallet } from '@meshsdk/core';
-import { newContext, yaciProvider } from '../../context';
+import { getCagingScript, newContext, yaciProvider } from '../../context';
 import { promises as fsPromises } from 'fs';
+import { Indexer } from '../../history/indexer';
+import { TrieManager } from '../../trie';
 
-// export async function setup(port: number) {
-//     // Ensure the tmp directory is clean before starting
-//     const tmpDir = `tmp/${port}`;
-//     await fsPromises.rm(tmpDir, { recursive: true, force: true });
-//     const mnemonic = generateMnemonic();
+export async function setup(port: number) {
+    // Ensure the tmp directory is clean before starting
+    const tmpDir = `tmp/${port}`;
+    await fsPromises.rm(tmpDir, { recursive: true, force: true });
+    const mnemonic = generateMnemonic();
 
-//     const mkWallet = provider =>
-//         new MeshWallet({
-//             networkId: 0,
-//             fetcher: provider,
-//             submitter: provider,
-//             key: {
-//                 type: 'mnemonic',
-//                 words: mnemonic.split(' ')
-//             }
-//         });
-//     const ctxProvider = yaciProvider(
-//         'http://localhost:8080',
-//         'http://localhost:10000'
-//     );
-//     const wallet = mkWallet(ctxProvider.provider);
-//     const context = await newContext(tmpDir, ctxProvider, wallet);
-//     if (ctxProvider.topup) {
-//         const { walletAddress } = await context.wallet();
-//         await ctxProvider.topup(walletAddress, 10_000);
-//     }
-//     return context;
-// }
+    const mkWallet = provider =>
+        new MeshWallet({
+            networkId: 0,
+            fetcher: provider,
+            submitter: provider,
+            key: {
+                type: 'mnemonic',
+                words: mnemonic.split(' ')
+            }
+        });
+    const ctxProvider = yaciProvider(
+        'http://localhost:8080',
+        'http://localhost:10000'
+    );
+    const ogmios = 'http://localhost:1337';
+    const wallet = mkWallet(ctxProvider.provider);
+    const tries = await TrieManager.create(tmpDir);
+
+    const { address, policyId } = getCagingScript();
+
+    const indexer = Indexer.create(
+        tries,
+        tmpDir,
+        address,
+        policyId,
+        ogmios,
+        'test-service' + port.toString()
+    );
+
+    new Promise(async () => {
+        await indexer.run();
+    });
+
+    const context = await newContext(indexer, ctxProvider, wallet);
+    if (ctxProvider.topup) {
+        const { walletAddress } = await context.wallet();
+        await ctxProvider.topup(walletAddress, 10_000);
+    }
+    return { context, close: async () => indexer.closeConnection() };
+}
+export async function sync(context) {
+    while (!(await context.indexerStatus())) {
+        console.log('Waiting for indexer to be ready...');
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+}
